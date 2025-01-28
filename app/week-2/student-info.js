8197427ef91b0a8cea21fabd45659c091367e175
Harkaran Singh <harkaran2004.jpr@gmail.com>
2025-01-28 02:40:04 -0700

    some changes made
    made some changes to the code

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Harkaran Singh</p>
+        <p>Link to Github: 
+        <Link href="https://github.com/karan-sidhu22/cprg306-assignments.git">My Github</Link>    
+        </p>    
+      </main>
+    );
+  }
