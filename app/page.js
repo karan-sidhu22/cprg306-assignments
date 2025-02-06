2d3ce44729d6e75ca2fc300a7b2b7c50bcf71100
Harkaran Singh <harkaran2004.jpr@gmail.com>
2025-02-06 16:11:26 -0700

    week-4 done

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 
-export default function page(){
-  return(
-    <div>
-      <h1> CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
-        Link to week 2: <Link href="week-2">Week 2</Link>
-      </p>
-    </div>
+export default function Page() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <ul>
+        <li><Link href="/week-2">Week 2 Assignment</Link></li>
+        <li><Link href="/week-3">Week 3 Assignment</Link></li>
+        <li><Link href="/week-4">Week 4 Assignment</Link></li>
+      </ul>
+    </main>
   );
 }
